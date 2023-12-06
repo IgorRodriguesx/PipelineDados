f7b07d8b76448784a528ec1ca8c0be5e6f54d628
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2023-12-06 11:02:12 -0300

    Create Lendo_Dados.py

---
+++ Python/Lendo_Dados.py
@@ -0,0 +1,6 @@
+path_json = '../data_raw/dados_empresaA.json'
+
+# Função Open para abrir os arquivos # Parametro para ler arquivo > path_json # 'r' de Read apenas para ler # As > 'apelido' para ler o arquivo # Armazenado na variavel File
+
+with open(path_json, 'r') as file:
+    print(file.readline())           # > Leitura dos dados 
