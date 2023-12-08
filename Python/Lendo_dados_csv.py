39188286a628afd4677ae5e2a928d61dcd1aa2b7
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2023-12-08 18:30:57 -0300

    Update Lendo_dados_csv.py

--- Python/Lendo_dados_csv.py
+++ Python/Lendo_dados_csv.py
@@ -13,5 +13,18 @@ with open(path_csv , 'r') as file:
 # armazenando dados na variavel 'dados_csv'
 with open(path_csv , 'r') as file:
     dados_csv = file.readlines()
+
+# Importar para poder ler o CSv
+import csv
+
+# reader capaz de fazr leitura
+with open(path_csv , 'r') as file:
+    dados_csv = file.readlines()
+
+# Fazer laço de repetição com o For para ler todas as linhas
+with open(path_csv , 'r') as file:
+    spamreader= csv.reader(file, delimiter=',')
+    for row in spamreader:        # Dentro desse FOr , percorrer todos os valores que o spamreader puder assumir e ir atribuindo ao row
+        print(row)
   
   
