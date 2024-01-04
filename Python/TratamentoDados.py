ebcc12ae4a581167c84157f89e690544ec4b858b
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-01-04 11:11:54 -0300

    Create TratamentoDados.py

---
+++ Python/TratamentoDados.py
@@ -0,0 +1,3 @@
+# Sobrescrevendo a variavel para ser combined_list acessando o ultimo valor (-1) onde estão os arquivos CSV com a data da compra pedindo as chaves .keys , tudo isso dentro de uma lista (list) 
+# Atualizar o nome da coluna com o nome da compra
+nomes_colunas = list(combined_list[-1].keys())
