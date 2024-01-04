f6387c4fec7697d098a506d2dad6ade2252d9929
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-01-04 14:25:32 -0300

    Update TratamentoDados.py

--- Python/TratamentoDados.py
+++ Python/TratamentoDados.py
@@ -1,3 +1,26 @@
 # Sobrescrevendo a variavel para ser combined_list acessando o ultimo valor (-1) onde estão os arquivos CSV com a data da compra pedindo as chaves .keys , tudo isso dentro de uma lista (list) 
 # Atualizar o nome da coluna com o nome da compra
 nomes_colunas = list(combined_list[-1].keys())
+nomes_colunas
+# Pegou o ultimo registro da lista combinada e adicionou na variavel sobrescrita
+
+# ANTES
+    ['Nome do Produto',
+ 'Categoria do Produto',
+ 'Preço do Produto (R$)',
+ 'Quantidade em Estoque',
+ 'Filial']
+
+# DEPOIS                             
+['Nome do Produto',
+ 'Categoria do Produto',
+ 'Preço do Produto (R$)',
+ 'Quantidade em Estoque',
+ 'Filial', 
+ 'Data da Venda']
+
+
+
+# lista de listas
+# Primeiro registro será o nome das colunas
+dados_combinados_tabela = [nomes_colunas]
