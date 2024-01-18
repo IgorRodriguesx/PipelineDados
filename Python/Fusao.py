b8ce3c74939ef3e0a469d6bc7e3791e24eff4709
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-01-18 16:03:42 -0300

    Update Fusao.py

--- Python/Fusao.py
+++ Python/Fusao.py
@@ -15,9 +15,11 @@ def leitura_csv(path_csv):
         spamreader= csv.DictReader(file, delimiter=',')
         for row in spamreader:     
             dados_csv.append(row)    
+    
     return dados_csv
 
 def leitura_dados(path, tipo_arquivo):
+    dados = []
     # Condicional verificando tipo do arquivo
     if tipo_arquivo == 'csv':
         dados = leitura_csv(path)
@@ -32,8 +34,8 @@ path_json = 'projeto_Requests/data_raw/dados_empresaA.json'
 path_csv = 'projeto_Requests/data_raw/dados_empresaB.csv'
 
 # Leitura dos dados Json e Csv
-dados_json = leitura_json(path_json)
+dados_json = leitura_dados(path_json, 'json')
 print(dados_json[0])
 
-dados_csv = leitura_csv(path_csv)
+dados_csv = leitura_dados(path_csv, 'csv')
 print(dados_csv[0])
