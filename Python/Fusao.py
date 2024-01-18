15efd289be32191f2dc6ed487b3990e74eb4d31d
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-01-18 17:24:27 -0300

    Update Fusao.py

--- Python/Fusao.py
+++ Python/Fusao.py
@@ -18,7 +18,7 @@ def leitura_csv(path_csv):
     
     return dados_csv
 
-def leitura_dados(path, tipo_arquivo): # Parametros da função (path , tipo_arquivo)
+def leitura_dados(path, tipo_arquivo):
     dados = []
     # Condicional verificando tipo do arquivo
     if tipo_arquivo == 'csv':
@@ -29,13 +29,38 @@ def leitura_dados(path, tipo_arquivo): # Parametros da função (path , tipo_arq
 
     return dados    
 
+def get_columns(dados):
+    return list(dados[0].keys())
+
+
 # Puxando os locais dos arquivos
 path_json = 'projeto_Requests/data_raw/dados_empresaA.json'
 path_csv = 'projeto_Requests/data_raw/dados_empresaB.csv'
 
 # Leitura dos dados Json e Csv
 dados_json = leitura_dados(path_json, 'json')
-print(dados_json[0])
+nome_colunas_json = get_columns(dados_json)
+print(nome_colunas_json)
 
 dados_csv = leitura_dados(path_csv, 'csv')
-print(dados_csv[0])
+nome_colunas_csv = get_columns(dados_csv)
+print(nome_colunas_csv)
+
+key_mapping = {'Nome do Item' : 'Nome do Produto',
+                'Classificação do Produto': 'Categoria do Produto',
+                'Valor em Reais (R$)': 'Preço do Produto (R$)',
+                'Quantidade em Estoque': 'Quantidade em Estoque',
+                'Nome da Loja': 'Filial',
+                'Data da Venda': 'Data da Venda'}
+
+# Transformação dos dados
+
+def rename_columns(dados, key_mapping):
+    new_dados_csv = []
+    
+    # Para cada registro na variável dados_csv
+    for old_dict in dados_csv:
+        dict_temp = {}
+        for old_key , value in old_dict.items():
+            dict_temp[key_mapping[old_key]] = value
+        new_dados_csv.[0]
