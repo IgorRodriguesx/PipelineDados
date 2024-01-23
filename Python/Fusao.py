3933d9a2092d40c69940134fc2ddd55b9b79dcec
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2024-01-23 14:55:14 -0300

    Update Fusao.py

--- Python/Fusao.py
+++ Python/Fusao.py
@@ -1,14 +1,13 @@
 import json
 import csv
 
-# Função
+# Funções
 def leitura_json(path_json):
     dados_json = []
     with open(path_json, 'r') as file:
         dados_json = json.load(file)
         return dados_json
 
- # Função   
 def leitura_csv(path_csv):
     dados_csv = []
     with open(path_csv , 'r') as file:
@@ -44,24 +43,37 @@ def rename_columns(dados, key_mapping):
 
     return new_dados_csv
 
+def size_data(dados):
+    return len(dados)
+
+def join(dadosA, dadosB):
+    combined_list = []
+    combined_list.extend(dadosA)
+    combined_list.extend(dadosB)
+    return combined_list
 
 # Puxando os locais dos arquivos
 path_json = 'projeto_Requests/data_raw/dados_empresaA.json'
 path_csv = 'projeto_Requests/data_raw/dados_empresaB.csv'
 
-# Leitura dos dados Json e Csv
+# Leitura dos dados Json
 dados_json = leitura_dados(path_json, 'json')
 nome_colunas_json = get_columns(dados_json)
-print(f"Nome colunas dados Json: {nome_colunas_json}")
+tamanho_dados_json = size_data(dados_json)
+print(f"Nome colunas dados json: {nome_colunas_json}")
+print(f"tamanho dos dados json:{tamanho_dados_json}")
 
+# Leitura dos dados Csv
 dados_csv = leitura_dados(path_csv, 'csv')
 nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+tamanho_dados_csv = size_data(dados_csv)
+print(f"Nome colunas dados csv: {nome_colunas_csv}")
+print(f"tamanho dos dados json:{tamanho_dados_csv}")
 
 key_mapping = {'Nome do Item' : 'Nome do Produto',
-                'Classificação do Produto': 'Categoria do Produto',
                 'Valor em Reais (R$)': 'Preço do Produto (R$)',
-                'Quantidade em Estoque': 'Quantidade em Estoque',
+               'Classificação do Produto': 'Categoria do Produto',
+                 'Quantidade em Estoque': 'Quantidade em Estoque',
                 'Nome da Loja': 'Filial',
                 'Data da Venda': 'Data da Venda'}
 
@@ -70,3 +82,5 @@ key_mapping = {'Nome do Item' : 'Nome do Produto',
 dados_csv = rename_columns(dados_csv, key_mapping)
 nome_colunas_csv = get_columns(dados_csv)
 print(nome_colunas_csv)
+
+dados_fusão = join(dados_json, dados_csv)
